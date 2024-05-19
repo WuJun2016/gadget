5aafb568cf4f25c70351b0891c2886aa43e5cef4
wujun <897415845@qq.com>
2024-05-19 17:16:42 +0800

    feat: 在浏览页面移除登录框提示

--- zhihu-redirect.user.js
+++ zhihu-redirect.user.js
@@ -4,16 +4,48 @@
 // @version      0.1
 // @description  重定向知乎登录页面到发现页面
 // @author       wj
-// @match        https://www.zhihu.com/signin?*
+// @run-at       document-idle
+// @match        https://www.zhihu.com/*
 // @grant        none
 // ==/UserScript==
 //
 (function () {
   "use strict";
 
+  // 功能一：重定向知乎登录页面到发现页面
   // 检查当前URL是否包含特定的字符串
   if (window.location.href.indexOf("https://www.zhihu.com/signin?") !== -1) {
     // 如果URL匹配，跳转到指定页面
     window.location.href = "https://www.zhihu.com/explore";
+  }
+
+  if (window.location.href.indexOf("https://www.zhihu.com") !== -1) {
+    // 功能二：移除知乎登录页面的登录框
+
+    function checkAndTriggerTilde() {
+      let modalDiv = document.querySelector(
+        "div.Modal.Modal--default.signFlowModal"
+      );
+
+      if (modalDiv) {
+        console.log("用户未登录，移除登录框");
+        let modalButton = document.querySelector(
+          "button.Button.Modal-closeButton"
+        );
+        modalButton.click();
+      }
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+      checkAndTriggerTilde();
+    });
+
+    let observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        checkAndTriggerTilde();
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
   }
 })();
